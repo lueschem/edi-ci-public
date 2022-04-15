2e603f5362bcb857e7a87f6b03bd0c0e0faf6782
Matthias Lüscher <lueschem@gmail.com>
2022-04-15 20:04:50 +0200

    Improved stability of ntp test.

--- tests/test_ntp.py
+++ tests/test_ntp.py
@@ -1,7 +1,15 @@
-import pytest
+import time
 
 
 def test_ntp_sync(host):
-    cmd = host.run("timedatectl")
-    assert cmd.rc == 0
+    retry = 12
+    sleep = 15
+    for i in range(retry):
+        cmd = host.run("timedatectl")
+        assert cmd.rc == 0
+        if "System clock synchronized: yes" in cmd.stdout:
+            return
+        elif i + 1 != retry:
+            time.sleep(sleep)
+
     assert "System clock synchronized: yes" in cmd.stdout
