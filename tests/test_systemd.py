6323447147dfa726440f6ac2df5a3f79b4154e2f
Matthias Luescher <lueschem@gmail.com>
2023-04-28 23:01:46 +0200

    Added systemd setup verification test.

--- tests/test_systemd.py
+++ tests/test_systemd.py
@@ -2,3 +2,10 @@ def test_systemd_overall_status(host):
     cmd = host.run("systemctl is-system-running")
     assert cmd.rc == 0
     assert "running" in cmd.stdout
+
+
+def test_systemd_analyze_verify(host):
+    cmd = host.run("systemd-analyze verify --man=no default.target")
+
+    assert cmd.stdout == ""
+    assert cmd.stderr == ""
