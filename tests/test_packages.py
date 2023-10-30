3c7abd8f2e58f609ad36b02569729f355ae1e647
Matthias Luescher <lueschem@gmail.com>
2023-10-30 22:38:16 +0100

    Make sure that the packages required for the disk resizing got installed.

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -1,7 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("name", ["edi-boot-shim", "systemd-timesyncd", ])
+@pytest.mark.parametrize("name", ["edi-boot-shim",
+                                  "systemd-timesyncd",
+                                  "fdisk",
+                                  "parted",])
 def test_packages(host, name):
     pkg = host.package(name)
     assert pkg.is_installed
